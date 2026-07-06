4e56ba93084febd78c2633c84d89b6dd558b89d0
camillothiago02-lang <camillothiago02@gmail.com>
2026-07-06 10:46:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("ORG PINGUIM BOT iniciado!");
+const {
+  Client,
+  GatewayIntentBits
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ ${client.user.tag} está online!`);
+});
+
+client.login(process.env.TOKEN);
